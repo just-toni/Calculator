77e25ffa6490a7d140569e3757d0a46d1c6ea80b
just-toni <75427368+just-toni@users.noreply.github.com>
2023-07-17 18:34:08 -0300

    Created operate function for the calculator

--- main.js
+++ main.js
@@ -14,4 +14,16 @@ function divide(a,b){
     return a/b;
 }
 
-let digit1, operator, digit2
+let digit1, operator, digit2;
+
+const operate = (digit1, operator, digit2) =>{
+    if(operator === '+'){
+        add(digit1, digit2);
+    } else if(operator === '-'){
+        subtract(digit1, digit2);
+    }else if(operator === '*'){
+        multiply(digit1, digit2);
+    }else if(operator === '/'){
+        divide(digit1, digit2);
+    }
+};
